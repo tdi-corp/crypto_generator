1ed3e9a8e664b2dfd6c3789c36b0d90fdba8f616
Dmitry T <d1mmas28@gmail.com>
2024-03-24 17:42:11 +0300

    add HelmetProvider

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,14 @@ import { store } from './store/store'
 import { Provider } from 'react-redux'
 import router from './routes'
 import './index.css'
+import { HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </HelmetProvider>
   </React.StrictMode>,
 )
