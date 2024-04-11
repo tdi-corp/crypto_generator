feef02503220f2b39dfd3cf61cc1279f53e149d4
Dmitry T <d1mmas28@gmail.com>
2024-04-11 19:45:57 +0300

    add github icon at Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom"
+import { GitHubLogoIcon } from "@radix-ui/react-icons"
+import { Button } from "./ui/Button"
 
 const Navbar = () => {
     return (
         <>
             <header className="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent">
-                <div className="mx-auto max-w-7xl px-4 flex justify-between gap-4 h-16 items-center">
+                <div className="mx-auto max-w-7xl px-4 flex justify-between h-16 items-center">
                     <div className="flex items-center gap-x-14">
                         <div>
                             logotype
@@ -20,8 +22,15 @@ const Navbar = () => {
                             </ul>
                         </nav>
                     </div>
-                    <div>
-                     
+                    <div className="flex justify-end">
+                        <Button 
+                            variant="ghost"
+                            onClick={() => {}}
+                        >
+                            <Link to="https://github.com/tdi-corp/crypto_generator" target="_blank">
+                                <GitHubLogoIcon className="w-4 h-4"/>
+                            </Link>
+                        </Button>
                     </div>
                 </div>
             </header>
